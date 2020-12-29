19c31c34b6f9c688e2c49265a777db3e9add5ae0
Ed <edwardtang@Edwards-MacBook-Pro.local>
2020-12-28 22:05:42 -0800

    chapter 32 start

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Edward Tang on 12/28/20.
+//  Copyright © 2020 Edward Tang. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
