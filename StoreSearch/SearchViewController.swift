5d16b5230118d7b723083a64246e55b62815c38e
Ed <edwardtang@Edwards-MacBook-Pro.local>
2020-12-29 07:53:05 -0800

    chapter 32 before UI Imrpovement, p.874

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Edward Tang on 12/28/20.
+//  Copyright © 2020 Edward Tang. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchResults = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+    }
+
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell:UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
+    }
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake result %d for '%@'", i, searchBar.text!))
+        }
+        tableView.reloadData()
+    }
+}
