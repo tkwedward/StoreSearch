2424bb1e1984329b37cddf300e170efffc959dcc
Ed <edwardtang@Edwards-MacBook-Pro.local>
2020-12-29 12:23:15 -0800

    chapter 33 before Changing the look of the app, p.900

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,27 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Edward Tang on 12/29/20.
+//  Copyright © 2020 Edward Tang. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
