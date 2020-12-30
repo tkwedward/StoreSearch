806b9ca096a19e71e45efbec8e89146e4144d1b1
Ed <edwardtang@Edwards-MacBook-Pro.local>
2020-12-29 17:48:40 -0800

    chapter 34. before showing the product type, p.944

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        customizeAppearance()
         return true
     }
 
@@ -27,6 +27,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    // MARK: - Helper Methods
+    func customizeAppearance() {
+        let barTintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        UISearchBar.appearance().barTintColor = barTintColor
     }
 
 
--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -8,8 +8,67 @@
 
 import Foundation
 
-class SearchResult {
-    var name = ""
-    var artistName = ""
+class ResultArray: Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
+}
+
+class SearchResult: Codable, CustomStringConvertible {
+    var artistName: String? = ""
+    var trackName: String?  = ""
+    var kind: String? = ""
     
+    
+    var trackPrice: Double? = 0.0
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+    
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
+    
+    var name: String {
+        return trackName ?? collectionName ?? ""
+    }
+    
+    var storeURL: String{
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+    
+    var price: Double{
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+    
+    var genre: String {
+        if let genre = itemGenre {
+            return genre
+        } else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        } else {
+            return ""
+        }
+    }
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
+    }
+    
+    var description: String {
+//        return "Name: \(name), Artist Name: \(artistName ?? "None")"
+        return "Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")\n"
+        
+    }
 }
