3c10ff9cdccb22baa7d11dd3ecb48f41a66755dc
Ed <edwardtang@Edwards-MacBook-Pro.local>
2020-12-30 17:15:20 -0800

    ch36 end, p.991

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -12,6 +12,8 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
+    
+    var downloadTask: URLSessionDownloadTask?
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,5 +28,25 @@ class SearchResultCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func configure(for result: SearchResult){
+        nameLabel.text = result.name
+        if result.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artist, result.type)
+        }
+        artworkImageView.image = UIImage(named: "Placeholder")
+        if let smallURL = URL(string: result.imageSmall) {
+            downloadTask = artworkImageView.loadImage(url: smallURL)
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        downloadTask?.cancel()
+        downloadTask = nil
+    }
+    
 
 }
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -128,14 +128,8 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
             let searchResult = searchResults[indexPath.row]
-            cell.nameLabel.text = searchResult.name
             
-            if searchResult.artist.isEmpty {
-                cell.artistNameLabel.text = "unknown"
-            } else {
-                cell.artistNameLabel.text = String(format: "%@ (%@)", searchResult.artist, searchResult.type)
-            }
-            
+            cell.configure(for: searchResult)
             return cell
         }
     }
